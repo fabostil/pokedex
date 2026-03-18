13bfbe04ac8f4cde3ac3bad5c1f84e55bddb19d6
fabostil <fabinbin7.000@gmail.com>
2026-03-18 19:55:19 -0300

    Projeto inicial Dart

---
+++ pokedex/bin/pokedex.dart
@@ -0,0 +1,5 @@
+import 'package:pokedex/pokedex.dart' as pokedex;
+
+void main(List<String> arguments) {
+  print('Hello world: ${pokedex.calculate()}!');
+}
---
+++ pokedex/test/pokedex_test.dart
@@ -0,0 +1,8 @@
+import 'package:pokedex/pokedex.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
